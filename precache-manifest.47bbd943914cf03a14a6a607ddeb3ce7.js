fb0d5637b2677068029ee821cc012d341706e8d1
Luis Felipe <luis14_felipe@hotmail.com>
2020-09-22 12:38:53 -0300

    Updates

---
+++ precache-manifest.47bbd943914cf03a14a6a607ddeb3ce7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa435d18065372d0b7ca4f990c5de1be",
+    "url": "/lf/index.html"
+  },
+  {
+    "revision": "4537553de553b9612789",
+    "url": "/lf/static/css/main.6f9628ce.chunk.css"
+  },
+  {
+    "revision": "7b4024da9db57665f356",
+    "url": "/lf/static/js/2.64e0ac6d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lf/static/js/2.64e0ac6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4537553de553b9612789",
+    "url": "/lf/static/js/main.1a092803.chunk.js"
+  },
+  {
+    "revision": "6c5c94b2dcc805291367",
+    "url": "/lf/static/js/runtime-main.6c55eb13.js"
+  }
+]);
